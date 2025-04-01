b9a9d659ee31d5924c2060fc948fb9f46cd90917
yash lekhwani~ <yashlekhwani49@gmail.com>
2025-04-01 11:40:04 +0530

    bug fix

--- habittracker/src/main.jsx
+++ habittracker/src/main.jsx
@@ -2,7 +2,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import {BrowserRouter} from "react-router-dom"
-import { HabitContextProvider } from './context/Context.jsx'
+import { HabitContextProvider } from './Context/HabitContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
